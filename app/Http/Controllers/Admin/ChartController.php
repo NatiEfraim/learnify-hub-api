3bc1afbcbf766497f9b39333014eb20a2261f760
NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>
2023-12-07 23:18:37 +0200

    add getallchart function

--- app/Http/Controllers/Admin/ChartController.php
+++ app/Http/Controllers/Admin/ChartController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chart;
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
     //
+    public function onAllSelect()
+    {
+        $result = Chart::all(); ///get all data from the tabel.
+        return $result;
+    }
 }
